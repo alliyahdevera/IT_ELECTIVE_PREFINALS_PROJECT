2aa862555e806a6139463a7c9bbb6868c1fab972
alliyahdevera <alliyahalceldevera@gmail.com>
2026-08-20 13:49:03 +0800

    US-010
    TicketAssignment, TicketComment, TicketAttachment, TicketTag entities

---
+++ HelpDesk/Models/TicketAssignment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HelpDesk.Models
+{
+    [Table("TicketAssignments")]
+    public class TicketAssignment
+    {
+        public int TicketId { get; set; }
+        public int EmployeeId { get; set; }
+        [Required] public DateTime AssignedAt { get; set; }
+        public DateTime? UnassignedAt { get; set; }
+        public bool IsPrimary { get; set; }
+
+        [ForeignKey(nameof(TicketId))] public Ticket Ticket { get; set; } = null!;
+        [ForeignKey(nameof(EmployeeId))] public Employee Employee { get; set; } = null!;
+    }
+}
---
+++ HelpDesk/Models/TicketAttachment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HelpDesk.Models
+{
+    [Table("TicketAttachments")]
+    public class TicketAttachment
+    {
+        [Key] public int Id { get; set; }
+        [Required] public int TicketId { get; set; }
+        [Required] public string FileName { get; set; } = string.Empty;
+        [Required] public string ContentType { get; set; } = string.Empty;
+        [Required] public long FileSize { get; set; }
+        [Required] public DateTime UploadedAt { get; set; }
+
+        [ForeignKey(nameof(TicketId))] public Ticket Ticket { get; set; } = null!;
+    }
+}
