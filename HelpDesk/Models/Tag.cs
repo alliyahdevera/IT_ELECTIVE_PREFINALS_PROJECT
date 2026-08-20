bac71add260e084771a709e3abedb11dda8fae94
alliyahdevera <alliyahalceldevera@gmail.com>
2026-08-20 13:20:39 +0800

    US-006: Tag entity (Added TeamMember)

---
+++ HelpDesk/Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HelpDesk.Models
+{
+    [Table("Tags")]
+    public class Tag
+    {
+        [Key] public int Id { get; set; }
+        [Required] public string Name { get; set; } = string.Empty;
+
+        public ICollection<TicketTag> TicketTags { get; set; } = new List<TicketTag>();
+    }
+}
