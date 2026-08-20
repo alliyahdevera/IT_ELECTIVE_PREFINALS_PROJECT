297e864bf88ae033edd45483c9c2fe0c28868186
alliyahdevera <alliyahalceldevera@gmail.com>
2026-08-20 13:27:44 +0800

    US-008: TicketPriority & TicketStatus entities

---
+++ HelpDesk/Models/TicketPriority.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HelpDesk.Models
+{
+    [Table("TicketPriorities")]
+    public class TicketPriority
+    {
+        [Key] public int Id { get; set; }
+        [Required] public string Name { get; set; } = string.Empty;
+        public int SortOrder { get; set; }
+        public int ResponseHours { get; set; }
+
+        public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+    }
+}
